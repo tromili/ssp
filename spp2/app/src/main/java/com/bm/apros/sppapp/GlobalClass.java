98dcaa40c87572f0225136b57ec6dbbc881c6cce
hanjelito <jgonzales@peruyoung.com>
2015-02-11 15:57:30 -0500

    actualizar

--- spp2/app/src/main/java/com/bm/apros/sppapp/GlobalClass.java
+++ spp2/app/src/main/java/com/bm/apros/sppapp/GlobalClass.java
@@ -6,4 +6,22 @@ package com.bm.apros.sppapp;
 import android.app.Application;
 
 public class GlobalClass extends Application {
+    private String Nombre;
+    private String Correo;
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return Correo;
+    }
+
+    public void setCorreo(String correo) {
+        Correo = correo;
+    }
 }
